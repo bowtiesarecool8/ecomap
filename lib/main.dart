9ebdc797111d3a950d86c57968993ad307d67185
bowtiesarecool8 <yoni@complexnp.com>
2022-09-29 12:42:38 +0300

    fixing stuff and firebase setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 
-void main() {
+import 'package:firebase_core/firebase_core.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
